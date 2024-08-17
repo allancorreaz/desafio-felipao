b60f1bb23eb3db511ce6cfbd7008b59e7a209fa9
allancorreaz <allancorreaz_@outlook.com>
2024-08-17 17:59:05 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-let nome = "Allan"
-console.log (nome);
+let nome = "Naruto";
+let xp_heroi = 120000;
+let nivel;
+
+if (xp_heroi< 1000){
+    nivel = "Ferro";
+} else if (xp_heroi >= 1001 && xp_heroi <= 2000){
+    nivel = "Bronze";
+}else if (xp_heroi >= 2001 && xp_heroi <= 5.000){
+    nivel = "Prata";
+} else if (xp_heroi >= 5001 && xp_heroi <= 7.000){
+    nivel = "Ouro";
+} else if (xp_heroi >= 7001 && xp_heroi <= 8.000){
+    nivel = "Platina";
+} else if (xp_heroi >= 8001 && xp_heroi <= 9.000){
+    nivel = "Ascendente";
+}else if (xp_heroi >= 9001 && xp_heroi <= 10000){ 
+    nivel = "Imortal";
+} else if (xp_heroi >= 10001 && xp_heroi <= 11000){
+    nivel = "Radiante";
+} else {xp_heroi >= 11001
+    nivel = "hokage e sanin lendário do mundo ninja"
+}
+
+console.log("O herói " + nome, "está no nível " + nivel)
