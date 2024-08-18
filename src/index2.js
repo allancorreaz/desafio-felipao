b65ad628d4fab71fd2c58159d51180b0cbcabc5c
allancorreaz <allancorreaz_@outlook.com>
2024-08-18 11:04:30 -0300

    Código Finalizado com switch case e else if em dois arquivos

---
+++ src/index2.js
@@ -0,0 +1,34 @@
+let nome = "Naruto";
+let xp_heroi = 120000;
+let nivel;
+
+switch (xp_heroi){
+    case xp_heroi < 1000: 
+    nivel = "Ferro"
+
+    case xp_heroi >= 1001 && xp_heroi <= 2000:
+    nivel = "Bronze";
+
+    case xp_heroi >= 2001 && xp_heroi <= 5.000:
+    nivel = "Prata";
+    
+    case xp_heroi >= 5001 && xp_heroi <= 7.000:
+    nivel = "Ouro";
+    
+    case xp_heroi >= 7001 && xp_heroi <= 8.000:
+    nivel = "Platina";
+
+    case xp_heroi >= 8001 && xp_heroi <= 9.000:
+    nivel = "Ascendente";
+
+    case xp_heroi >= 9001 && xp_heroi <= 10000:
+    nivel = "Imortal";
+
+    case xp_heroi >= 10001 && xp_heroi <= 11000:
+    nivel = "Radiante";
+    
+    default:
+    nivel = "hokage e sanin lendário do mundo ninja"
+
+
+console.log("O herói " + nome, "está no nível " + nivel)}
